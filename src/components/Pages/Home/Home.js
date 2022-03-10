d01dbabca6eed12952aeea98a575cf3b579082a3
MDSELIM1 <selimakodo58@gmail.com>
2022-03-11 00:18:25 +0600

    add

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../../Shared/Banner/Banner";
+import LittleAbout from "./LittleAbout/LittleAbout";
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <LittleAbout />
     </div>
   );
 };
---
+++ src/components/Pages/Home/LittleAbout/LittleAbout.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./LittleAbout.css";
+
+const LittleAbout = () => {
+  return (
+    <div className="littleabout_maindiv">
+      <h1 className="littleabout_title">THE RESTAURANT</h1>
+      <h4 className="littleabout_para">
+        A little about us and a breif history of how we started.
+      </h4>
+      <div className="bar_div_main">
+        <div className="bar_div_inner"></div>
+      </div>
+    </div>
+  );
+};
+
+export default LittleAbout;
