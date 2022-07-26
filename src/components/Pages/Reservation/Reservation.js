f3ea6d39ee895d51c9f6d8c1d23668cec4716031
MDSELIM1 <selimakodo58@gmail.com>
2022-07-27 00:40:15 +0600

    add

--- src/components/Pages/Reservation/Reservation.js
+++ src/components/Pages/Reservation/Reservation.js
@@ -2,15 +2,19 @@ import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import DatePicker from "react-datepicker";
 import { addDays } from "date-fns";
-import "react-datepicker/dist/react-datepicker.css";
 import { useState } from "react";
+import TimeInput from "react-time-picker-input";
 import { useForm } from "react-hook-form";
+import "react-datepicker/dist/react-datepicker.css";
 import "./Reservation.css";
+
 const Reservation = () => {
   const [startDate, setStartDate] = useState(null);
   const { register, handleSubmit } = useForm();
+  const [timeInput, setTimeInput] = useState("10:12");
+
   const onSubmit = (data) => {
-    console.log(data);
+    console.log(data, startDate, timeInput);
   };
 
   return (
@@ -28,8 +32,8 @@ const Reservation = () => {
           </div>
           <div className="border p-5">
             <form onSubmit={handleSubmit(onSubmit)}>
-              <Row>
-                <Col md={4}>
+              <Row className="py-5">
+                <Col md={4} className="mb-5">
                   <label htmlFor="datepicker" className="mb-2">
                     Date
                   </label>
@@ -41,9 +45,10 @@ const Reservation = () => {
                     maxDate={addDays(new Date(), 10)}
                     placeholderText="Pick a date for your order"
                     className="w-100 py-2 px-3"
+                    required
                   />
                 </Col>
-                <Col md={4}>
+                <Col md={4} className="mb-5">
                   <label htmlFor="name" className="mb-2">
                     Name
                   </label>
@@ -53,8 +58,63 @@ const Reservation = () => {
                     type="text"
                     {...register("name")}
                     placeholder="Enter your full name"
+                    required
                   />
                 </Col>
+                <Col md={4} className="mb-5" style={{ zIndex: 3 }}>
+                  <label htmlFor="time" className="mb-2">
+                    Time
+                  </label>
+                  <TimeInput
+                    value={timeInput}
+                    hour12Format
+                    manuallyDisplayDropdown
+                    eachInputDropdown
+                    onChange={(dateString) => setTimeInput(dateString)}
+                  />
+                </Col>
+                <Col md={4}>
+                  <label htmlFor="email" className="mb-2">
+                    E-mail
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    required
+                    placeholder="Enter your email here"
+                    className="w-100 py-2 px-3"
+                  />
+                </Col>
+                <Col md={4}>
+                  <label htmlFor="guest" className="mb-2">
+                    Guest
+                  </label>
+                  <input
+                    type="number"
+                    className="w-100 py-2 px-3"
+                    id="guest"
+                    required
+                    placeholder="How many people enter number "
+                  />
+                </Col>
+                <Col md={4}>
+                  <label htmlFor="numbermobile" className="mb-2">
+                    {" "}
+                    Mobile number
+                  </label>
+                  <input
+                    type="number"
+                    className="w-100 py-2 px-3"
+                    placeholder="Enter your mobile number"
+                    required
+                    id="numbermobile"
+                  />
+                </Col>
+                <div className="text-center mt-5">
+                  <button type="submit" className="w-auto item_button">
+                    Make Reservation
+                  </button>
+                </div>
               </Row>
             </form>
           </div>
